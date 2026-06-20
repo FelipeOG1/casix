f23ac2c62deffc330106e276748f494e26ccc969
FelipeOG1 <feliog1234@gmail.com>
2026-06-20 16:44:46 -0600

    first instructions disk loader

--- boot_sect_disk.asm
+++ boot_sect_disk.asm
@@ -1,1 +1,15 @@
-
+load_from_disk:
+    pusha
+    push dx
+    
+    mov ah, 0x02   ;ah-> mode(0x02 read mode)
+    mov al, dh     ;al->number of sectors
+    mov cl, 0x02   ;cl->sector number
+    
+    mov ch, 0x00   ;ch->cylinder number
+    
+    mov dh, 0x00   ;dh-> head number
+    
+    int 0x13;      ;BIOS DISK INTERRUPT
+    
+    
