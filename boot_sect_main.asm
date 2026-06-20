cf54a74a0a3e90be555bfe31a3b63d175b7d4dfa
FelipeOG1 <feliog1234@gmail.com>
2026-06-20 15:48:53 -0600

    init disk reader

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,10 +1,24 @@
-[org 0x7c00] ;bootloader offset
+mov ah, 0x0e ; tty
+mov bx, 0x7c0
+mov ds, bx
+mov al, [the_secret]
+int 0x10
 
-mov dx, 0x12fe
-call print_hex
+
+mov al, 'Z'
+int 0x10
 
 
-%include "boot_sect_print.asm"
-%include "boot_sect_printhex.asm"
+
+jmp $
+
+the_secret:
+    db "X"
+
+
+
+SECRET:
+    db 'X'
+
 times 510-($-$$) db 0
 dw 0xaa55
