1f1501d83e7f38badb1b04558b02751c90aed755
FelipeOG1 <feliog1234@gmail.com>
2026-06-20 14:44:34 -0600

    improve print label instructions

--- boot_sect_main.asm
+++ boot_sect_main.asm
@@ -1,27 +1,13 @@
 [org 0x7c00] ;bootloader offset
-
-
 mov bx, HELLO
-call print
-call println
-mov bx, BYE
-call print
-call println
-mov bx, FELIPE
 call print
 jmp $
 
 
-%include "boot_sect_print.asm"
-
 HELLO:
     db 'Hello', 0
 
-BYE:
-    db 'Bye', 0
-
-FELIPE:
-    db 'Felipe', 0
+%include "boot_sect_print.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
--- boot_sect_print.asm
+++ boot_sect_print.asm
@@ -1,33 +1,18 @@
 ; int 0x10 trigers video interrupt
 ; mov ah, 0x0e enters tty mode 
-
-
 print:
     pusha
-    
-start: ; increment bx contents by 1 pointer until null char is found (0)
+    mov ah, 0x0e
+
+start:
     mov al, [bx]
     cmp al, 0
     je done
 
-    mov ah, 0x0e 
-    int 0x10 
-    
+    int 0x10
     add bx, 1
     jmp start
 
 done:
-    popa
-    ret
-
-
-println:
-    pusha
-    
-    mov ah, 0x0e
-    mov al, 0x0a; newline char
-    int 0x10
-    mov al, 0x0d ; move cursor to start line
-    int 0x10
     popa
     ret
