eaa42ca1988a17145cd680d08ad6fa960b0f1f8a
FelipeOG1 <feliog1234@gmail.com>
2026-06-21 20:21:04 -0600

    start gdt table setup

---
+++ 32_bit/32bit_gdt.asm
@@ -0,0 +1,33 @@
+;EACH SEGMENT IS  8 BYTES LONG
+
+
+gdt_start:
+    dd 0x00
+    dd 0x00
+
+gdt_code:
+    dw 0xffff
+    dw 0x00
+    db 0x00
+    db 10011010b ; flags (8 bits)
+    db 11001111b ; flags (4 bits) + segment length, bits 16-19
+    db 0x0       ; segment base, bits 24-31
+
+
+
+gdt_data:
+    dw dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+    
+
+gdt_descriptor:
+    
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
--- bootloader/boot_sect_main.asm
+++ bootloader/boot_sect_main.asm
@@ -1,8 +1,15 @@
 [org 0x7c00]
 
 
-mov bx, HELLO
-call print
+
+
+
+mov bx, 0x9000
+mov cl, 0x03
+call read_disk
+
+mov dx, [bx]
+call print_hex
 
 jmp $
 HELLO:
@@ -11,6 +18,13 @@ HELLO:
 
 
 %include "bootloader/boot_sect_print.asm"
+%include "bootloader/boot_sect_disk.asm"
+%include "bootloader/boot_sect_printhex.asm"
+
 
 times 510 -($-$$) db 0
 dw 0xaa55
+
+times 512 db 5
+times 512 db 10
+
