6de1899fff845bfafaa3939d0013cb679f5db277
FelipeOG1 <feliog1234@gmail.com>
2026-06-27 21:11:54 -0600

    refactor to display have own cursor position

--- drivers/screen.cpp
+++ drivers/screen.cpp
@@ -1,9 +1,10 @@
 #include "ports.h"
 #include "screen.h"
 namespace ScreenDriver{
-    volatile char* vga = (volatile char*)0xb8000;
     
-    int get_cursor_position() {
+    volatile char* vga = (volatile char*)0xb8000;
+
+    int Cursor::get_position() {
         Ports::write_port_byte(Ports::VgaPorts::PORT_VGA, 14); // request HIGH BYTE
         int position = Ports::read_port_byte(Ports::VgaPorts::PORT_VGA + 1) << 8; // read and store high byte
         Ports::write_port_byte(Ports::VgaPorts::PORT_VGA, 15); // request LOW BYTE
@@ -11,16 +12,26 @@ namespace ScreenDriver{
         return position;
     }
     
+    void Cursor::update_position() {
+        Cursor::_position += 1;
+        Ports::write_port_byte(Ports::VgaPorts::PORT_VGA, 14);
+        Ports::write_port_byte(Ports::VgaPorts::PORT_VGA + 1,
+                               (Cursor::_position >> 8) & 0xFF);
+
+        Ports::write_port_byte(Ports::VgaPorts::PORT_VGA, 15);
+        Ports::write_port_byte(Ports::VgaPorts::PORT_VGA + 1, 
+                               Cursor::_position & 0xFF);
+    }
     
-    void write_char(char value, 
-                    ScreenDriver::Color background,
-                    ScreenDriver::Color foreground) {
+    void Display::write_char(char value, 
+                    ScreenDriver::VgaColor background,
+                    ScreenDriver::VgaColor foreground) {
     
-        int offset_vga = get_cursor_position() * 2;
+        int offset_vga = Display::_cursor.position() * 2;
         unsigned char color_attr = (background << 4) | foreground; 
         vga[offset_vga] = value;
         vga[offset_vga+1] = color_attr;
-        
+        Display::_cursor.update_position();
     }
 
 
--- drivers/screen.h
+++ drivers/screen.h
@@ -1,14 +1,32 @@
 namespace ScreenDriver{
-    
-    enum Color: unsigned char {
+    enum VgaColor: unsigned char {
         BLACK = 0,
         BLUE = 1,
         
     };
-    int get_cursor_position();
-    void write_char(char value, 
-                    ScreenDriver::Color background,
-                    ScreenDriver::Color foreground);
+    class Cursor {
+    private:
+        int _position;
+        
+    public:
+        int get_position();
+        void update_position();
+        Cursor() : _position(get_position()) {}
+
+        int position() const { return _position; }
+    }; 
+       
+    class Display {
+        Cursor _cursor;
+        
+    public:
+        void write_char(char value, 
+                    ScreenDriver::VgaColor background,
+                    ScreenDriver::VgaColor foreground);
+
+        Display() : _cursor(Cursor()) {}
     
+    };
+   
 
 }
